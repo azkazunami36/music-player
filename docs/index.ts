488981b5e74adf3a47a7f5bbdefd62660e1d275b
azkazunami36 <kazunami36.sum@icloud.com>
2025-03-08 09:34:09 +0900

    誤字を修正

--- docs/index.ts
+++ docs/index.ts
@@ -3,7 +3,7 @@ import HTMLGeneratorHub from "./js/HTMLGeneratorHub.js";
 const button = new HTMLGeneratorHub.HTMLButtonGenerator();
 
 button.text = "テスト";
-button.imageURL = "IMG_4712.p"
+button.imageURL = "IMG_4712.png";
 button.click(() => { button.border = !button.border; });
 
 document.body.appendChild(button.html);
