d5997f98acbf8f91c8eadd0c621816e5d79c1e95
azkazunami36 <kazunami36.sum@icloud.com>
2025-03-08 09:27:07 +0900

    画像表示テスト

--- docs/index.ts
+++ docs/index.ts
@@ -3,8 +3,7 @@ import HTMLGeneratorHub from "./js/HTMLGeneratorHub.js";
 const button = new HTMLGeneratorHub.HTMLButtonGenerator();
 
 button.text = "テスト";
-button.click(() => {
-    button.border = !button.border;
-})
+button.imageURL = "IMG_4712.png";
+button.click(() => { button.border = !button.border; });
 
 document.body.appendChild(button.html);
